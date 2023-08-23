6d7e063b6cb05c367a93f3fbccdf183a5ca904a8
lubej <9722540+lubej@users.noreply.github.com>
2023-08-23 12:52:25 +0200

    Add disconnect and exchange tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,10 @@
 import BleTransport from "../index";
 import {ScanResult, BleClientInterface} from "@capacitor-community/bluetooth-le";
 
+interface Timeout extends NodeJS.Timeout {
+  _destroyed: boolean
+}
+
 jest.mock("@capacitor-community/bluetooth-le", () => {
   let startNotificationCb = (_: DataView) => {
   };
@@ -28,19 +32,32 @@ jest.mock("@capacitor-community/bluetooth-le", () => {
       },
       writeWithoutResponse(deviceId: string, service: string, characteristic: string, value: DataView): Promise<void> {
         const hex = Buffer.from(value.buffer, value.byteOffset, value.byteLength).toString('hex')
-        let returnValue: DataView;
+        let buffer: Buffer;
 
         switch (hex) {
           // MTU handshake
           case "0800000000":
-            const buffer = Buffer.from("080000000199", "hex");
-            returnValue = new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength);
+            buffer = Buffer.from("080000000199", "hex");
             break;
+          // getAppAndVersion
+          case "0500000005b010000000":
+            buffer = Buffer.from(
+              "05000000130105424f4c4f5309312e302e302d7263399000",
+              "hex",
+            );
+            break;
+          // just used for a non resolving apdu
+          case "0500000005b020000000":
+            setTimeout(() => {
+              buffer = Buffer.from("05000000029000", "hex");
+              startNotificationCb(new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength))
+            }, 600);
+            return Promise.resolve();
           default:
             throw new Error('Unexpected input')
         }
 
-        startNotificationCb(returnValue)
+        startNotificationCb(new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength))
         return Promise.resolve()
       }
     } satisfies Partial<BleClientInterface>
@@ -48,7 +65,7 @@ jest.mock("@capacitor-community/bluetooth-le", () => {
 });
 
 describe("BleTransport connectivity test coverage", () => {
-  const device: ScanResult = {
+  const scanResult: ScanResult = {
     device: {
       deviceId: 'XX:XX:XX:XX:XX:XX',
       name: 'Nano X BXAX',
@@ -61,14 +78,99 @@ describe("BleTransport connectivity test coverage", () => {
 
   describe("Device available and already paired", () => {
     it("should find the device, connect, negotiate MTU", async () => {
-      const transport = await BleTransport.open(device);
+      const transport = await BleTransport.open(scanResult);
       expect(transport.isConnected).toBe(true);
     });
 
     it("should be disconnectable, and cleanup", async () => {
-      const transport = await BleTransport.open(device);
-      await BleTransport.disconnect(device.device.deviceId)
+      const transport = await BleTransport.open(scanResult);
+      await BleTransport.disconnect(scanResult.device.deviceId)
       expect(transport.isConnected).toBe(false);
+    });
+
+    it("should disconnect in 500ms (5s default) after calling close", async () => {
+      const transport = await BleTransport.open(scanResult);
+      expect(transport.isConnected).toBe(true);
+
+      BleTransport.disconnectTimeoutMs = 500;
+      await transport.close();
+
+      // Expect the timeout for disconnection to be set
+      expect(transport.disconnectTimeout).not.toBe(undefined);
+      let resolve: (value: void) => void;
+
+      transport.on("disconnect", () => {
+        resolve();
+      });
+
+      return await new Promise((_resolve, _reject) => {
+        resolve = _resolve;
+      });
+    });
+
+    it("should cancel disconnect if new connection is made", async () => {
+      const transport = await BleTransport.open(scanResult);
+      expect(transport.isConnected).toBe(true);
+
+      BleTransport.disconnectTimeoutMs = 500;
+      await transport.close();
+
+      expect(transport.disconnectTimeout).not.toBe(undefined);
+      expect((transport.disconnectTimeout as Timeout)._destroyed).toBe(false);
+      await BleTransport.open(scanResult);
+      expect((transport.disconnectTimeout as Timeout)._destroyed).toBe(true);
+    });
+
+    it("should cancel disconnect if already disconnected", async () => {
+      const transport = await BleTransport.open(scanResult);
+      expect(transport.isConnected).toBe(true);
+
+      BleTransport.disconnectTimeoutMs = 500;
+      await transport.close();
+
+      expect(transport.disconnectTimeout).not.toBe(undefined);
+      expect((transport.disconnectTimeout as Timeout)._destroyed).toBe(false);
+      await BleTransport.disconnect(scanResult.device.deviceId);
+      expect((transport.disconnectTimeout as Timeout)._destroyed).toBe(true);
+    });
+
+    it("should handle exchanges if all goes well", async () => {
+      const transport = await BleTransport.open(scanResult);
+      expect(transport.isConnected).toBe(true);
+
+      const response = await transport.exchange(Buffer.from("b010000000", "hex"));
+      expect(response.toString("hex")).toBe("0105424f4c4f5309312e302e302d7263399000");
+    });
+
+    it("should throw on exchanges if disconnected", async () => {
+      const transport = await BleTransport.open(scanResult);
+      expect(transport.isConnected).toBe(true);
+      await BleTransport.disconnect(scanResult.device.deviceId);
+      expect(transport.isConnected).toBe(false);
+      await expect(transport.exchange(Buffer.from("b010000000", "hex"))).rejects.toThrow(); // More specific errors some day.
+    });
+
+    it("should disconnect if close is called, even if pending response", done => {
+      // This is actually a very important test, if we have an ongoing apdu response,
+      // as in, the device never replied, but we expressed the intention of disconnecting
+      // we will give it a few seconds and then disconnect regardless. Otherwise we fall
+      // in the never ending await trap.
+      async function asyncFn() {
+        const transport = await BleTransport.open(scanResult);
+        expect(transport.isConnected).toBe(true);
+        transport.exchange(Buffer.from("b020000000", "hex"));
+        BleTransport.disconnectTimeoutMs = 500;
+
+        transport.on("disconnect", () => {
+          done(); // If this is never called, then we're still waiting.
+        });
+        await transport.close();
+
+        // Expect the timeout for disconnection to be set
+        expect(transport.disconnectTimeout).not.toBe(undefined);
+      }
+
+      asyncFn();
     });
   });
 });
